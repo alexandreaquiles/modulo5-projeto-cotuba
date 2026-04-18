0ba308bac2f03073936a367448efca17b01f475b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2026-04-18 16:28:50 -0300

    LSP + Encapsulamento

--- estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/ContadorPalavras.java
+++ estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/ContadorPalavras.java
@@ -1,11 +1,38 @@
 package br.com.unipds.estatisticas;
 
+import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeMap;
 
-class ContadorPalavras extends TreeMap<String, Integer> {
+class ContadorPalavras implements Iterable<ContadorPalavras.ContagemPalavra> {
+
+    record ContagemPalavra (String palavra, int ocorrencias) {}
+
+    private Map<String, Integer> mapa = new TreeMap<>();
 
     public void adicionarPalavra(String palavra) {
-        super.merge(palavra, 1, Integer::sum);
+        mapa.merge(palavra, 1, Integer::sum);
+    }
+
+    public Iterable<? extends Map.Entry<String, Integer>> entrySet() {
+        return mapa.entrySet();
+    }
+
+    @Override
+    public Iterator<ContagemPalavra> iterator() {
+        Iterator<Map.Entry<String, Integer>> iterator = mapa.entrySet().iterator();
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public ContagemPalavra next() {
+                Map.Entry<String, Integer> entry = iterator.next();
+                return new ContagemPalavra(entry.getKey(), entry.getValue());
+            }
+        };
     }
 
 }
--- estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/PluginEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/PluginEstatisticas.java
@@ -3,10 +3,9 @@ package br.com.unipds.estatisticas;
 import br.com.unipds.cotuba.domain.Capitulo;
 import br.com.unipds.cotuba.domain.Ebook;
 import br.com.unipds.cotuba.plugin.CotubaPlugin;
+import br.com.unipds.estatisticas.ContadorPalavras.ContagemPalavra;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-import java.util.Map;
 
 public class PluginEstatisticas implements CotubaPlugin  {
 
@@ -31,12 +30,9 @@ public class PluginEstatisticas implements CotubaPlugin  {
             }
         }
 
-        for (Map.Entry<String, Integer> contagem : contadorPalavras.entrySet()) {
-            String palavra = contagem.getKey();
-            Integer ocorrencias = contagem.getValue();
-            System.out.printf("'%s': %d\n", palavra, ocorrencias);
+        for (ContagemPalavra contagem : contadorPalavras) {
+            System.out.printf("'%s': %d\n", contagem.palavra(), contagem.ocorrencias());
         }
-
 
     }
 }
